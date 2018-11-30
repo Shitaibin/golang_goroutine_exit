8e4d2f8d33af9cf6c1089f712a3252a18ec52e71
shitaibin <hz_stb@163.com>
2018-11-30 17:21:38 +0800

    Using for-range exit goroutine

--- select.go
+++ select.go
@@ -5,31 +5,47 @@ import (
 	"time"
 )
 
-func worker(stopCh <-chan struct{}) {
+func producer(n int) <-chan int {
+	out := make(chan int)
 	go func() {
-		defer fmt.Println("worker exit")
+		defer func() {
+			close(out)
+			out = nil
+			fmt.Println("producer exit")
+		}()
 
-		t := time.NewTicker(time.Millisecond * 500)
-
-		for {
-			select {
-			case <-stopCh:
-				fmt.Println("Recv stop signal")
-				return
-			case <-t.C:
-				fmt.Println("Working .")
-			}
+		for i := 0; i < n; i++ {
+			fmt.Printf("send %d\n", i)
+			out <- i
+			time.Sleep(time.Millisecond)
 		}
 	}()
-	return
+	return out
+}
+
+func consumer(in <-chan int) <-chan struct{} {
+	finish := make(chan struct{})
+
+	go func() {
+		defer func() {
+			fmt.Println("worker exit")
+			finish <- struct{}{}
+		}()
+
+		// Using for-range to exit goroutine
+		for x := range in {
+			fmt.Printf("Process %d\n", x)
+		}
+	}()
+
+	return finish
 }
 
 func main() {
+	out := producer(3)
+	finish := consumer(out)
 
-	stopCh := make(chan struct{})
-	worker(stopCh)
-
-	time.Sleep(time.Second * 2)
-	close(stopCh)
+	// Wait consumer exit
+	<-finish
 	fmt.Println("main exit")
 }
